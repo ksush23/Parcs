b965cd9b158a6549d45b6d19978bc43221a8383b
ksush23 <47639130+ksush23@users.noreply.github.com>
2021-03-17 20:29:36 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -27,7 +27,7 @@ public class Main implements AM {
         for (File photo : photos) {
             point p = info.createPoint();
             channel c = p.createChannel();
-            p.execute("Algo.jar");
+            p.execute("Algo.java");
             c.write(photo);
             channels.add(c);
         }
