8060411c7d4586d71168c489fc80031f66b7feb9
ksush23 <47639130+ksush23@users.noreply.github.com>
2021-03-17 20:14:32 +0200

    Update Algo.java

--- src/Algo.java
+++ src/Algo.java
@@ -7,7 +7,7 @@ public class App implements AM {
 
     public static void main(String[] args) {
         task curtask = new task();
-        curtask.addJarFile("Main.jar");
+        curtask.addJarFile("Algo.jar");
         (new App()).run(new AMInfo(curtask, (channel)null));
         curtask.end();
     }
